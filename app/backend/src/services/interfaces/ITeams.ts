006edb230ae482deca4b51f54907a154e4bf6682
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-24 19:40:04 -0300

    :sparkles: feat: adicionando interfaces

--- app/backend/src/services/interfaces/ITeams.ts
+++ app/backend/src/services/interfaces/ITeams.ts
@@ -1,4 +1,31 @@
-export default interface ITeams {
+import Team from '../../database/models/Team';
+
+export interface ITeams {
   id: number,
   teamName: string,
+}
+
+export interface ITeamMatches extends Team {
+  id: number,
+  teamName: string,
+  teamHome: [],
+  awayTeam: number,
+  homeTeamGoals: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+}
+
+export interface ITeamResult {
+  id: number,
+  teamName: string,
+  teamHome: [
+    {
+      id: number,
+      homeTeam: number,
+      awayTeam: number,
+      homeTeamGoals: number,
+      awayTeamGoals: number,
+      inProgress: boolean,
+    },
+  ]
 }
