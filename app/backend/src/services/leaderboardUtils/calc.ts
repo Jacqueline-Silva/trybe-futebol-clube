78ba2f51b2f0fee02177e1098d2797916bb69466
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-25 13:02:14 -0300

    :recycle: refactor: add aos métodos um parametro que auxilia no retorno dinamico

--- app/backend/src/services/leaderboardUtils/calc.ts
+++ app/backend/src/services/leaderboardUtils/calc.ts
@@ -1,68 +1,76 @@
 import { ITeamMatches } from '../interfaces/ITeams';
 
 export default class CalcResults {
-  static totalPoints = (matches: ITeamMatches[]) => {
-    const result = (this.totalVictories(matches) * 3) + this.totalDraws(matches);
+  static totalPoints = (matches: ITeamMatches[], who: string) => {
+    const result = (this.totalVictories(matches, who) * 3) + this.totalDraws(matches, who);
     return result;
   };
 
   static totalGames = (matches: ITeamMatches[]): number => matches.length;
 
-  static totalVictories = (matches: ITeamMatches[]): number => {
+  static totalVictories = (matches: ITeamMatches[], who: string): number => {
     let victories = 0;
+    const team = who === 'home' ? 'homeTeamGoals' : 'awayTeamGoals';
+    const team2 = who === 'home' ? 'awayTeamGoals' : 'homeTeamGoals';
     matches.forEach((match) => {
-      if (match.homeTeamGoals > match.awayTeamGoals) {
+      if (+`${match[team]}` > +`${match[team2]}`) {
         victories += 1;
       }
     });
     return victories;
   };
 
-  static totalDraws = (matches: ITeamMatches[]): number => {
+  static totalDraws = (matches: ITeamMatches[], who: string): number => {
     let draws = 0;
+    const team = who === 'home' ? 'homeTeamGoals' : 'awayTeamGoals';
+    const team2 = who === 'home' ? 'awayTeamGoals' : 'homeTeamGoals';
     matches.forEach((match) => {
-      if (match.homeTeamGoals === match.awayTeamGoals) {
+      if (+(`${match[team]}`) === +(`${match[team2]}`)) {
         draws += 1;
       }
     });
     return draws;
   };
 
-  static totalLosses = (matches: ITeamMatches[]): number => {
+  static totalLosses = (matches: ITeamMatches[], who: string): number => {
     let losses = 0;
+    const team = who === 'home' ? 'homeTeamGoals' : 'awayTeamGoals';
+    const team2 = who === 'home' ? 'awayTeamGoals' : 'homeTeamGoals';
     matches.forEach((match) => {
-      if (match.homeTeamGoals < match.awayTeamGoals) {
+      if (+(`${match[team]}`) < +(`${match[team2]}`)) {
         losses += 1;
       }
     });
     return losses;
   };
 
-  static totalGoalsFavor = (matches: ITeamMatches[]): number => {
+  static totalGoalsFavor = (matches: ITeamMatches[], who: string): number => {
     let goals = 0;
+    const team = who === 'home' ? 'homeTeamGoals' : 'awayTeamGoals';
     matches.forEach((match) => {
-      goals += match.homeTeamGoals;
+      goals += +(`${match[team]}`);
     });
     return goals;
   };
 
-  static totalGoalsOwn = (matches: ITeamMatches[]): number => {
+  static totalGoalsOwn = (matches: ITeamMatches[], who: string): number => {
     let goals = 0;
+    const team2 = who === 'home' ? 'awayTeamGoals' : 'homeTeamGoals';
     matches.forEach((match) => {
-      goals += match.awayTeamGoals;
+      goals += +(`${match[team2]}`);
     });
     return goals;
   };
 
-  static totalEfficiency = (matches: ITeamMatches[]): number => {
-    const points = this.totalPoints(matches);
+  static totalEfficiency = (matches: ITeamMatches[], who:string): number => {
+    const points = this.totalPoints(matches, who);
     const games = this.totalGames(matches);
     const result = +(((points / (games * 3)) * 100).toFixed(2));
     return result;
   };
 
-  static totalGoalsBalance = (matches: ITeamMatches[]) => {
-    const result = this.totalGoalsFavor(matches) - this.totalGoalsOwn(matches);
+  static totalGoalsBalance = (matches: ITeamMatches[], who: string) => {
+    const result = this.totalGoalsFavor(matches, who) - this.totalGoalsOwn(matches, who);
     return result;
   };
 }
--- app/backend/src/services/leaderboardUtils/generate.ts
+++ app/backend/src/services/leaderboardUtils/generate.ts
@@ -3,35 +3,22 @@ import { ITeamMatches } from '../interfaces/ITeams';
 import { ITeamBoard } from '../interfaces/ILeaderboard';
 
 export default class LeardeboardGenerate {
-  static createBoardTeam = (matches: ITeamMatches[]): ITeamBoard[] => {
-    const result = matches.map((t) => ({
-      name: t.teamName,
-      totalPoints: CalcResults.totalPoints(t.teamHome),
-      totalGames: CalcResults.totalGames(t.teamHome),
-      totalVictories: CalcResults.totalVictories(t.teamHome),
-      totalDraws: CalcResults.totalDraws(t.teamHome),
-      totalLosses: CalcResults.totalLosses(t.teamHome),
-      goalsFavor: CalcResults.totalGoalsFavor(t.teamHome),
-      goalsOwn: CalcResults.totalGoalsOwn(t.teamHome),
-      goalsBalance: CalcResults.totalGoalsBalance(t.teamHome),
-      efficiency: CalcResults.totalEfficiency(t.teamHome),
-    }));
+  static createBoardTeam = (matches: ITeamMatches[], who: string): ITeamBoard[] => {
+    const result = matches.map((t) => {
+      const team = who === 'home' ? t.homeMatches : t.awayMatches;
+      return ({
+        name: t.teamName,
+        totalPoints: CalcResults.totalPoints(team, who),
+        totalGames: CalcResults.totalGames(team),
+        totalVictories: CalcResults.totalVictories(team, who),
+        totalDraws: CalcResults.totalDraws(team, who),
+        totalLosses: CalcResults.totalLosses(team, who),
+        goalsFavor: CalcResults.totalGoalsFavor(team, who),
+        goalsOwn: CalcResults.totalGoalsOwn(team, who),
+        goalsBalance: CalcResults.totalGoalsBalance(team, who),
+        efficiency: CalcResults.totalEfficiency(team, who),
+      });
+    });
     return result;
   };
-
-  // static generateTotalBoard = (home: any, away: any) => ({
-  // name: home.name,
-  // totalPoints: home.totalPoints + away.totalPoints,
-  // totalGames: home.totalGames + away.totalGames,
-  // totalVictories: home.totalVictories + away.totalVictories,
-  // totalDraws: home.totalDraws + away.totalDraws,
-  // totalLosses: home.totalLosses + away.totalLosses,
-  // goalsFavor: home.goalsFavor + away.goalsFavor,
-  // goalsOwn: home.goalsOwn + away.goalsOwn,
-  // goalsBalance: home.goalsBalance + away.goalsBalance,
-  // efficiency: CalcResults.totalEfficiency(
-  // (home.totalPoints + away.totalPoints),
-  // (home.totalGames + away.totalGames),
-  // ),
-  // });
 }
