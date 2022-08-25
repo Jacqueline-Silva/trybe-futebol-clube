1492a9e1b0d44e9b378d9579919e025791ad589a
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-25 13:04:55 -0300

    :recycle: refactor: altera "as" do model + add um novo parametro ao getBoardHome

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -7,13 +7,14 @@ import LeardeboardOrderBoard from './leaderboardUtils/order';
 export default class LeaderboardsService {
   static getHome = async () => {
     const result = await Team.findAll({
-      include: [{ model: Match, as: 'teamHome', where: { inProgress: false } }],
+      include: [
+        { model: Match, as: 'homeMatches', where: { inProgress: false } }],
     });
     return result as ITeamMatches[];
   };
 
   static getBoardHome = async () => {
-    const result = LeardeboardGenerate.createBoardTeam(await this.getHome());
+    const result = LeardeboardGenerate.createBoardTeam(await this.getHome(), 'home');
     const orderBy = LeardeboardOrderBoard.orderBoard(result);
     return orderBy;
   };
